a7e3bd7bed5c470c4a93228c7d3089e742354d1e
Ian Kollipara <ian.kollipara@gmail.com>
2024-03-28 09:06:25 -0500

    Add initialization for database storage folder

--- .fly/scripts/1_storage_init.sh
+++ .fly/scripts/1_storage_init.sh
@@ -5,3 +5,13 @@ if [ ! -d "$FOLDER" ]; then
     echo "deleting storage_..."
     rm -rf /var/www/html/storage_
 fi
+
+# .fly/scripts/1_storage_init.sh
+
+# Add this below the storage folder initialization snippet
+FOLDER=/var/www/html/storage/database
+if [ ! -d "$FOLDER" ]; then
+    echo "$FOLDER is not a directory, initializing database"
+    mkdir /var/www/html/storage/database
+    touch /var/www/html/storage/database/database.sqlite
+fi
